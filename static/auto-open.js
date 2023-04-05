1d68143568f648a7469d9b13b07222166aae75c5
Bart Nagel <bart@tremby.net>
2023-04-05 12:01:04 -0700

    Gone

--- static/auto-open.js
+++
@@ -1,9 +0,0 @@
-function openTarget() {
-	if (!window.location.hash) return;
-	const target = document.getElementById(window.location.hash.substring(1));
-	if (target == null) return;
-	const details = target.tagName === "DETAILS" ? target : target.querySelector("details");
-	if (details == null) return;
-	details.open = true;
-}
-openTarget();
